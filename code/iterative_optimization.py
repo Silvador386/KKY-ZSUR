a3788856fdbe206e0492658b27875b0227424705
Silvador386 <silvador.386@atlas.cz>
2022-11-12 17:54:26 +0100

    Corrected iterative optimization

--- code/iterative_optimization.py
+++ code/iterative_optimization.py
@@ -3,51 +3,51 @@ from utils import L2_distance_matrix
 from plot import plot_2D
 
 
+# def optimize(classed_data, center_data, plot=True):
+#     error_f = np.zeros(center_data.shape[0])
+#     for i, (cls_data, cls_center) in enumerate(zip(classed_data, center_data)):
+#         dists_matrix = L2_distance_matrix(cls_data, cls_center[:, np.newaxis].T)
+#         error_f[i] = np.sum(dists_matrix)
+#
+#     total_error = np.sum(error_f)
+#     old_total_error = -1
+#     while True:
+#         if total_error == old_total_error:
+#             break
+#         old_total_error = total_error
+#
+#         for i, _ in enumerate(center_data):
+#             for _, data_point in enumerate(classed_data[i]):
+#                 if len(classed_data[i]) == 1:
+#                     continue
+#                 A_i = len(classed_data[i]) / (len(classed_data[i]) - 1) * np.sum((data_point - center_data[i]) ** 2)
+#                 A_js_with_idx = []
+#                 for j, (cls_data2, cls_center2) in enumerate(zip(classed_data, center_data)):
+#                     if i == j:
+#                         continue
+#                     A_j = len(cls_data2) / (len(cls_data2) + 1) * np.sum((data_point - cls_center2)**2)
+#                     A_js_with_idx.append([A_j, j])
+#
+#                 A_min, A_min_idx = A_js_with_idx[np.argmin(np.array(A_js_with_idx)[:, 0])]
+#                 if A_i > A_min:
+#                     error_f[i] -= A_i
+#                     error_f[A_min_idx] += A_min
+#
+#                     classed_data[A_min_idx] = np.vstack([classed_data[A_min_idx], data_point])
+#                     classed_data[i] = np.delete(classed_data[i], np.argwhere(classed_data[i] == data_point), axis=0)
+#
+#                     center_data[i] = np.average(classed_data[i])
+#                     center_data[A_min_idx] = np.average(classed_data[A_min_idx])
+#         total_error = np.sum(error_f)
+#
+#     if plot:
+#         data2plot_named = {f"Center: {center[0]:.2f}, {center[1]:.2f}": data for data, center in zip(classed_data, center_data)}
+#         plot_2D(**data2plot_named)
+#
+#     return classed_data
+
+
 def optimize(classed_data, center_data, plot=True):
-    error_f = np.zeros(center_data.shape[0])
-    for i, (cls_data, cls_center) in enumerate(zip(classed_data, center_data)):
-        dists_matrix = L2_distance_matrix(cls_data, cls_center[:, np.newaxis].T)
-        error_f[i] = np.sum(dists_matrix)
-
-    total_error = np.sum(error_f)
-    old_total_error = -1
-    while True:
-        if total_error == old_total_error:
-            break
-        old_total_error = total_error
-
-        for i, _ in enumerate(center_data):
-            for _, data_point in enumerate(classed_data[i]):
-                if len(classed_data[i]) == 1:
-                    continue
-                A_i = len(classed_data[i]) / (len(classed_data[i]) - 1) * np.sum((data_point - center_data[i]) ** 2)
-                A_js_with_idx = []
-                for j, (cls_data2, cls_center2) in enumerate(zip(classed_data, center_data)):
-                    if i == j:
-                        continue
-                    A_j = len(cls_data2) / (len(cls_data2) + 1) * np.sum((data_point - cls_center2)**2)
-                    A_js_with_idx.append([A_j, j])
-
-                A_min, A_min_idx = A_js_with_idx[np.argmin(np.array(A_js_with_idx)[:, 0])]
-                if A_i > A_min:
-                    error_f[i] -= A_i
-                    error_f[A_min_idx] += A_min
-
-                    classed_data[A_min_idx] = np.vstack([classed_data[A_min_idx], data_point])
-                    classed_data[i] = np.delete(classed_data[i], np.argwhere(classed_data[i] == data_point), axis=0)
-
-                    center_data[i] = np.average(classed_data[i])
-                    center_data[A_min_idx] = np.average(classed_data[A_min_idx])
-        total_error = np.sum(error_f)
-
-    if plot:
-        data2plot_named = {f"Center: {center[0]:.2f}, {center[1]:.2f}": data for data, center in zip(classed_data, center_data)}
-        plot_2D(**data2plot_named)
-
-    return classed_data
-
-
-def opt(classed_data, center_data, plot=True):
     data = np.array([np.append(row, cls_idx) for cls_idx, data_class in enumerate(classed_data) for row in data_class])
     num_R = len(center_data)
     cls_sizes = np.array([cls_data.shape[0] for cls_data in classed_data])
@@ -90,7 +90,8 @@ def opt(classed_data, center_data, plot=True):
                 center_data[dp_cls] = np.average(data[np.where(data[:, -1] == dp_cls)][:, :-1], axis=0)
                 center_data[A_j_cls] = np.average(data[np.where(data[:, -1] == A_j_cls)][:, :-1], axis=0)
 
-    classed_data = np.split(data, np.where(np.diff(data[:, -1]))[0] + 1)
+    data = data[data[:, -1].argsort()]
+    classed_data = np.split(data[:, :-1], np.where(np.diff(data[:,-1]))[0]+1)
 
     if plot:
         data2plot_named = {f"Center: {center[0]:.2f}, {center[1]:.2f}": data for data, center in zip(classed_data, center_data)}
