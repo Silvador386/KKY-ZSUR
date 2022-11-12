8e36be2244b3ece264d1396b0a10cfd32bfae2f5
Silvador386 <silvador.386@atlas.cz>
2022-11-12 17:39:08 +0100

    Made iterative optimization

--- code/iterative_optimization.py
+++ code/iterative_optimization.py
@@ -1,41 +1,100 @@
 import numpy as np
 from utils import L2_distance_matrix
+from plot import plot_2D
 
 
-def optimize(clustered_data, center_data):
-    num_cls = len(clustered_data)
+def optimize(classed_data, center_data, plot=True):
     error_f = np.zeros(center_data.shape[0])
-    for i, (cls_data, cls_center) in enumerate(zip(clustered_data, center_data)):
+    for i, (cls_data, cls_center) in enumerate(zip(classed_data, center_data)):
         dists_matrix = L2_distance_matrix(cls_data, cls_center[:, np.newaxis].T)
         error_f[i] = np.sum(dists_matrix)
+
     total_error = np.sum(error_f)
+    old_total_error = -1
+    while True:
+        if total_error == old_total_error:
+            break
+        old_total_error = total_error
 
-    for i, (cls_data, cls_center) in enumerate(zip(clustered_data, center_data)):
-        for dp_idx, data_point in enumerate(cls_data):
-            if len(cls_data) == 1:
-                continue
-            A_i = len(cls_data)/(len(cls_data)-1) * np.sum(data_point**2 - cls_center**2)
-            A_js_with_idx = []
-            for j, (cls_data2, cls_center2) in enumerate(zip(clustered_data, center_data)):
-                if i == j:
+        for i, _ in enumerate(center_data):
+            for _, data_point in enumerate(classed_data[i]):
+                if len(classed_data[i]) == 1:
                     continue
-                A_j = len(cls_data2) / (len(cls_data2) + 1) * np.sum(data_point ** 2 - cls_center2 ** 2)
-                A_js_with_idx.append([A_j, j])
+                A_i = len(classed_data[i]) / (len(classed_data[i]) - 1) * np.sum((data_point - center_data[i]) ** 2)
+                A_js_with_idx = []
+                for j, (cls_data2, cls_center2) in enumerate(zip(classed_data, center_data)):
+                    if i == j:
+                        continue
+                    A_j = len(cls_data2) / (len(cls_data2) + 1) * np.sum((data_point - cls_center2)**2)
+                    A_js_with_idx.append([A_j, j])
 
-            A_min = A_js_with_idx[np.argmin(np.array(A_js_with_idx)[:, ])]
-            if A_i > A_min[0]:
-                clustered_data[A_min[1]] = np.vstack([clustered_data[A_min[1]], data_point])
-                cls_data = np.delete(cls_data, dp_idx)
+                A_min, A_min_idx = A_js_with_idx[np.argmin(np.array(A_js_with_idx)[:, 0])]
+                if A_i > A_min:
+                    error_f[i] -= A_i
+                    error_f[A_min_idx] += A_min
+
+                    classed_data[A_min_idx] = np.vstack([classed_data[A_min_idx], data_point])
+                    classed_data[i] = np.delete(classed_data[i], np.argwhere(classed_data[i] == data_point), axis=0)
+
+                    center_data[i] = np.average(classed_data[i])
+                    center_data[A_min_idx] = np.average(classed_data[A_min_idx])
+        total_error = np.sum(error_f)
+
+    if plot:
+        data2plot_named = {f"Center: {center[0]:.2f}, {center[1]:.2f}": data for data, center in zip(classed_data, center_data)}
+        plot_2D(**data2plot_named)
+
+    return classed_data
 
 
-                cls_center = np.average(cls_data)
-                center_data[A_min[1]] = np.average(clustered_data[A_min[1]])
+def opt(classed_data, center_data, plot=True):
+    data = np.array([np.append(row, cls_idx) for cls_idx, data_class in enumerate(classed_data) for row in data_class])
+    num_R = len(center_data)
+    cls_sizes = np.array([cls_data.shape[0] for cls_data in classed_data])
 
-                error_f2 = np.zeros(center_data.shape[0])
-                for i3, (cls_data3, cls_center3) in enumerate(zip(clustered_data, center_data)):
-                    dists_matrix = L2_distance_matrix(cls_data3, cls_center3[:, np.newaxis].T)
-                    error_f2[i] = np.sum(dists_matrix)
+    cls_error = np.zeros(center_data.shape[0])
+    for i, (cls_data, cls_center) in enumerate(zip(classed_data, center_data)):
+        dists_matrix = L2_distance_matrix(cls_data, cls_center[:, np.newaxis].T)
+        cls_error[i] = np.sum(dists_matrix)
 
+    previous_error = -1
+    while True:
+        total_error = np.sum(cls_error)
+        if total_error == previous_error:
+            break
+        previous_error = total_error
 
-    return clustered_data
+        for dp_idx, data_point in enumerate(data):
+            dp_cls = int(data_point[2])
+
+            if cls_sizes[dp_cls] == 1:
+                continue
+
+            dists = L2_distance_matrix(data_point[:2, np.newaxis].T, center_data)
+            change_v = [-1 if i == dp_cls else 1 for i in range(num_R)]
+            size_scale = cls_sizes / np.array(cls_sizes + change_v)
+
+            A = (size_scale * dists).reshape(-1)
+            A_i_val = A[dp_cls]
+            A_j_val = np.min(A[A != A_i_val])
+            A_j_cls = int(np.where(A == A_j_val)[0])
+
+            if A_i_val > A_j_val:
+                data[dp_idx][-1] = A_j_cls
+                cls_error[dp_cls] -= A_i_val
+                cls_error[A_j_cls] += A_j_val
+
+                cls_sizes[dp_cls] -= 1
+                cls_sizes[A_j_cls] += 1
+
+                center_data[dp_cls] = np.average(data[np.where(data[:, -1] == dp_cls)][:, :-1], axis=0)
+                center_data[A_j_cls] = np.average(data[np.where(data[:, -1] == A_j_cls)][:, :-1], axis=0)
+
+    classed_data = np.split(data, np.where(np.diff(data[:, -1]))[0] + 1)
+
+    if plot:
+        data2plot_named = {f"Center: {center[0]:.2f}, {center[1]:.2f}": data for data, center in zip(classed_data, center_data)}
+        plot_2D(**data2plot_named)
+
+    return classed_data
 
--- code/main.py
+++ code/main.py
@@ -3,7 +3,7 @@ import plot
 import numpy as np
 import clustering
 import k_means
-from iterative_optimization import optimize
+from iterative_optimization import optimize, opt
 
 
 DATA_PATH = "../data/data.txt"
@@ -15,11 +15,11 @@ def main():
     data_sample = data[np.random.randint(data.shape[0], size=1000)]
 
     # plot.plot_2D(args)
-    cls_count = 3
+    cls_count = 6
     # cls_count = clustering.run_cluster_methods(data_sample)
     print(f"Number of classes:{cls_count}")
     clustered_data, center_data = k_means.k_means_div(data_sample, cls_count, plot=True)
-    optimize(clustered_data, center_data)
+    opt(clustered_data, center_data)
 
 if __name__ == "__main__":
     main()
